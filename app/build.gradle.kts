294f494df848f05998aab57462715cfc4d989fab
Cirrus CI <support@cirruslabs.org>
2024-11-29 19:07:47 +0000

    Update versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.el3sas.zad"
         minSdk = 21
         targetSdk = 34
-        versionCode = 202
-        versionName = "v2.0.2"
+        versionCode = 203
+        versionName = "v2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
