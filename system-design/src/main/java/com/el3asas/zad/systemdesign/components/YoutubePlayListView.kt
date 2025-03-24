1a3aae57cdcb8f745294a26742f299cbe114f527
Ahmed Sheref <Ahmed.Sheref.3.96@gmail.com>
2025-03-24 02:28:45 +0200

    update YoutubePlayListView with web ui controller

--- system-design/src/main/java/com/el3asas/zad/systemdesign/components/YoutubePlayListView.kt
+++ system-design/src/main/java/com/el3asas/zad/systemdesign/components/YoutubePlayListView.kt
@@ -1,24 +1,34 @@
-package com.el3sas.zad.components
+package com.el3asas.zad.systemdesign.components
 
 import android.app.Activity
 import android.content.Context
+import android.view.View
 import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import com.el3sas.zad.utils.enterFullscreen
-import com.el3sas.zad.utils.exitFullscreen
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.customui.DefaultPlayerUiController
+import com.el3asas.zad.systemdesign.utils.enterFullscreen
+import com.el3asas.zad.systemdesign.utils.exitFullscreen
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.FullscreenListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.YouTubePlayerTracker
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
@@ -31,18 +41,20 @@ fun YoutubePlayListView(
     playlistId: String,
     onReady: (YouTubePlayer) -> Unit = {},
 ) {
-    val lifecycleOwner = LocalLifecycleOwner.current
     var youtubePlayer by remember { mutableStateOf<YouTubePlayer?>(null) }
     var isFullscreen by rememberSaveable { mutableStateOf(false) }
     val originalOrientation = rememberSaveable { activity.requestedOrientation }
+    var lastSavedVideoLocation by rememberSaveable { mutableFloatStateOf(0f) }
 
-    DisposableEffect(isFullscreen) {
+    LaunchedEffect(isFullscreen) {
         if (isFullscreen) {
             activity.enterFullscreen()
         } else {
             activity.exitFullscreen()
         }
+    }
 
+    DisposableEffect(Unit) {
         onDispose {
             activity.requestedOrientation = originalOrientation
         }
@@ -59,38 +71,52 @@ fun YoutubePlayListView(
             factory = { ctx ->
                 YouTubePlayerView(context).apply {
                     enableAutomaticInitialization = false
-//                    enableBackgroundPlayback(true)
+                    enableBackgroundPlayback(true)
                     val tracker = YouTubePlayerTracker()
 
                     val options =
                         IFramePlayerOptions
                             .Builder()
-                            .controls(0)
+                            .controls(1)
                             .listType("playlist")
                             .list(playlistId)
                             .fullscreen(1)
                             .build()
 
+                    addFullscreenListener(
+                        object : FullscreenListener {
+                            override fun onEnterFullscreen(
+                                fullscreenView: View,
+                                exitFullscreen: () -> Unit,
+                            ) {
+                                if (isFullscreen) {
+                                    exitFullscreen.invoke()
+                                } else {
+                                    lastSavedVideoLocation = tracker.currentSecond
+                                    isFullscreen = true
+                                }
+                            }
+
+                            override fun onExitFullscreen() {
+                                lastSavedVideoLocation = tracker.currentSecond
+                                isFullscreen = false
+                            }
+                        },
+                    )
+
                     initialize(
                         object : AbstractYouTubePlayerListener() {
                             override fun onReady(player: YouTubePlayer) {
                                 player.addListener(tracker)
-                                val defaultPlayerUiController =
-                                    DefaultPlayerUiController(this@apply, player)
-
-                                defaultPlayerUiController.setFullscreenButtonClickListener {
-                                    isFullscreen = !isFullscreen
-                                }
-                                this@apply.setCustomPlayerUi(defaultPlayerUiController.rootView)
                                 onReady(player)
                                 youtubePlayer = player
-                                player.playVideoAt(0)
-                                player.seekTo(tracker.currentSecond)
+                                player.seekTo(lastSavedVideoLocation)
+                                player.play()
                             }
                         },
+                        true,
                         options,
                     )
-                    lifecycleOwner.lifecycle.addObserver(this)
                 }
             },
             modifier =
